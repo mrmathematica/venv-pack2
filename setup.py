bbfbb84248261d830706d70200d38708bb8c98bf
Chongkai Zhu <czhu@cs.utah.edu>
2024-05-30 10:40:33 +0800

    Migrate from setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
 from setuptools import setup
 
-setup(name='venv-pack2',
-      version='0.7.0',
-      url='https://github.com/mrmathematica/venv-pack',
-      project_urls={"Source Code": "https://github.com/mrmathematica/venv-pack"},
-      maintainer='mrmathematica',
-      maintainer_email='mrmathematica@yahoo.com',
-      keywords='venv packaging',
-      classifiers=["License :: OSI Approved :: BSD License",
-                   "Programming Language :: Python :: 3",
-                   "Topic :: System :: Archiving :: Packaging",
-                   "Topic :: System :: Software Distribution",
-                   "Topic :: Software Development :: Build Tools"],
-      license='BSD',
-      description='Package virtual environments for redistribution',
-      long_description=open('README.md').read(),
-      long_description_content_type='text/markdown',
-      packages=['venv_pack'],
-      package_data={'venv_pack': ['scripts/*',
-                                  'scripts/common/*',
-                                  'scripts/nt/*']},
-      entry_points='''
-        [console_scripts]
-        venv-pack=venv_pack.__main__:main
-      ''',
-      zip_safe=False)
+setup()
